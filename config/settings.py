5d128a628185fb3ca826e8eba0e6e6aed1fb555a
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-08-13 15:21:32 -0500

    chore: Setup crispy form

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -144,3 +146,5 @@ AUTH_USER_MODEL='accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
