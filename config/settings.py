b1b655080db000e84aeee19c38373e64a6669972
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-08-17 17:31:30 -0500

    chore: Set up configurations for working with environment variables

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 import os
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
